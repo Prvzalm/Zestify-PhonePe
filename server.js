095c2bcdc2c58feec1c854c33a8332b404ca2243
Parvez Alam <prvzalm69@gmail.com>
2023-12-16 13:37:34 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const app = express();
 const port = 5000;
 const cors = require("cors");
 require("dotenv").config();
+const proxy = require('express-http-proxy')('http://localhost:3000');
 
 app.use(express.json());  
 app.use(cors());
 const bodyParser = require('body-parser');
+
+app.use(express.static('build'))
 
 // Parse JSON and URL-encoded form data
 app.use(bodyParser.json());
@@ -15,6 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Razorpay Route
 const phonepeRoute = require('./routes/phonepe/phonepeRoute')
 app.use("/api", phonepeRoute);
+
+//proxy for backend
+app.use('/api', proxy);
 
 // Starting Server
 app.listen(port, () => {
